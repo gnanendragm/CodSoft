427006ed02598bcf66721a23c3a235c9aac63fc0
gnanendragm <146975172+gnanendragm@users.noreply.github.com>
2023-12-09 02:06:59 +0530

    Update 2Calculator.cpp

--- 2Calculator.cpp
+++ 2Calculator.cpp
@@ -1,24 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
     float a, b;
-    char op;
-    
+    char op;  
     cout<<"Calculator!\n";
-    
     cout<<"Enter the 1st number: ";
     cin>>a;
-    
     cout<<"Enter the 2nd number: ";
     cin>>b;
     
     cout<<"Choose 1: \n1.Addition(+)\n2.Subtraction(-)\n3.Multiplication(*)\n4.Division(/)\n\n";
     cin>>op;
-    
-    switch(op)
-    {
+    switch(op){
         case '+':
             cout<<a<<" + "<<b<<" = "<<a+b;
             break;
